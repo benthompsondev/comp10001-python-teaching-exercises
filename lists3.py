ff0a0a501cdf23cc776c9e129bd03e6e46795c84
Benjamin Thompson <69312218+BenThompsonBTC@users.noreply.github.com>
2022-03-07 14:07:05 -0500

    Add files via upload

---
+++ lists3.py
@@ -0,0 +1,15 @@
+planets = ["mercury", "venus", "earth", "mars", "jupiter"]
+
+i = 0 
+while i < len(planets): 
+    print  (planets[i]) 
+    i = i+1 
+
+#Just adding some Spacing
+print ()
+
+print (planets [0]) 
+print (planets [1]) 
+print (planets [2]) 
+print (planets [3]) 
+print (planets [4]) 
---
+++ lst4.py
@@ -0,0 +1,3 @@
+numList=[10,40,-6,123,42]
+
+print(max(numList))
